a89a00d214631737729bc401e49ab9db809fef03
ashipan <60419784+ashipan@users.noreply.github.com>
2021-07-11 01:48:31 +0900

    Update insertSort.cpp

--- insertSort.cpp
+++ insertSort.cpp
@@ -1,6 +1,7 @@
-void insertionSort(vector<int>& a){
+template<typename T>
+void insertSort(vector<T>& a){
     for(int i = 1; i < a.size(); i++){
-        int v = a[i];
+        T v = a[i];
         int j = i-1;
         while(j >= 0 && a[j] > v){
             a[j+1] = a[j];
