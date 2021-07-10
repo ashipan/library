ca4223b53e631d922dc22d9a3eb9dbebd1a894ae
ashipan <60419784+ashipan@users.noreply.github.com>
2021-07-11 01:58:09 +0900

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,9 +1,9 @@
 template<typename T>
 void bubbleSort(vector<T>& a){
     bool flag = 1;
-    while(flag){
+    for(int i = 0; flag; i++){
         flag = 0;
-        for(int j = a.size()-1; j >= 1; j--){
+        for(int j = a.size()-1; j >= i+1; j--){
             if(a[j] < a[j-1]){
                 swap(a[j], a[j-1]);
                 flag = 1;
