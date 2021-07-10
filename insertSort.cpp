6f73d185783de638b8bf6b82aff524b81123423b
ashipan <60419784+ashipan@users.noreply.github.com>
2021-07-11 01:33:04 +0900

    Create insertSort.cpp

---
+++ insertSort.cpp
@@ -0,0 +1,11 @@
+void insertSort(vector<int>& a){
+    for(int i = 1; i < a.size(); i++){
+        int v = a[i];
+        int j = i-1;
+        while(j >= 0 && a[j] > v){
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = v;
+    }
+}
