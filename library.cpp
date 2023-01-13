af2cf7282ef816c19bfeb46c472efa98905faed5
ashipan <r.hujiyama_0518@icloud.com>
2023-01-13 22:00:36 +0900

    add perm

--- library.cpp
+++ library.cpp
@@ -1074,6 +1074,33 @@ vector<T> LIS(vector<T>& a){
   return dp;
 }
 
+//Perm
+struct Perm : vector<int> {
+#define n (int)(size())
+#define p (*this)
+  Perm(int _n): vector<int>(_n) { iota(begin(), end(), 0);}
+  template<class...Args> Perm(Args...args): vector<int>(args...) {}
+  Perm(initializer_list<int> a): vector<int>(a.begin(),a.end()) {}
+  Perm operator+(const Perm& a) const {
+    Perm r(n);
+    for (int i = 0; i < n; ++i) r[i] = p[a[i]];
+    return r;
+  }
+  Perm& operator+=(const Perm& a) { return *this = (*this)+a;}
+  Perm operator-() const {
+    Perm r(n);
+    for (int i = 0; i < n; ++i) r[p[i]] = i;
+    return r;
+  }
+  Perm operator-(const Perm& a) const { return *this + -a;}
+  Perm& operator-=(const Perm& a) { return *this += -a;}
+  // next permutation
+  bool operator++() { return next_permutation(begin(),end());}
+#undef n
+#undef p
+};
+
+
 //PERMUTATION_SUM
 ll permutation_sum(ll l, ll r, ll n){ return (l+r)*n/2;}
 mint mod_permutation_sum(ll l, ll r, ll n){ return mint(l+r)*n/2;}
