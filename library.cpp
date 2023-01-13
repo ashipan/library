1e5ba22ffe5cf22b5ed971749da0a4f4dc0eb4ab
ashipan <r.hujiyama_0518@icloud.com>
2023-01-13 19:54:42 +0900

    add RollingHash

--- library.cpp
+++ library.cpp
@@ -1338,6 +1338,62 @@ struct Rerooting{
 };
 
 
+//RollingHash
+struct RollingHash {
+  static const int base1 = 1007, base2 = 2009;
+  static const int mod1 = 1000000007, mod2 = 1000000009;
+  vector<long long> hash1, hash2, power1, power2;
+  RollingHash(const string &s) {
+    int n = (int)s.size();
+    hash1.assign(n+1, 0); hash2.assign(n+1, 0);
+    power1.assign(n+1, 1); power2.assign(n+1, 1);
+    for (int i = 0; i < n; i++) {
+      hash1[i+1] = (hash1[i]*base1 + s[i])%mod1;
+      hash2[i+1] = (hash2[i]*base2 + s[i])%mod2;
+      power1[i+1] = (power1[i]*base1)%mod1;
+      power2[i+1] = (power2[i]*base2)%mod2;
+    }
+  }
+  // get hash value of s[l:r]
+  inline long long get(int l, int r) const {
+    long long res1 = hash1[r] - hash1[l]*power1[r-l]%mod1;
+    long long res2 = hash2[r] - hash2[l]*power2[r-l]%mod2;
+    if (res1 < 0) res1 += mod1;
+    if (res2 < 0) res2 += mod2;
+    return res1*mod2 + res2;
+  }
+  
+  // get hash value of s
+  inline long long get() const {
+    return hash1.back()*mod2 + hash2.back();
+  }
+  
+  // get lcp of s[a:] and s[b:]
+  inline int getLCP(int a, int b) const {
+    int len = min((int)hash1.size()-a, (int)hash1.size()-b);
+    int low = 0, high = len;
+    while (high - low > 1) {
+      int mid = (low + high)>>1;
+      if (get(a, a+mid) != get(b, b+mid)) high = mid;
+      else low = mid;
+    }
+    return low;
+  }
+  
+  // get lcp of s[a:] and t[b:]
+  inline int getLCP(const RollingHash &t, int a, int b) const {
+    int len = min((int)hash1.size()-a, (int)hash1.size()-b);
+    int low = 0, high = len;
+    while (high - low > 1) {
+      int mid = (low + high)>>1;
+      if (get(a, a+mid) != t.get(b, b+mid)) high = mid;
+      else low = mid;
+    }
+    return low;
+  }
+};
+
+
 //SegmentTree
 template<class S, S (*op)(S, S), S (*e)()>
 struct SEG{
