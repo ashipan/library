abe001c846b75401e864c0761f16f4fa85460b1e
ashipan <r.hujiyama_0518@icloud.com>
2023-01-23 00:21:29 +0900

    fixed warshall

--- library.cpp
+++ library.cpp
@@ -2367,13 +2367,14 @@ struct WaveletMatrix{
 //Warshall_floyd
 bool negative_loop = false;
 template<typename T>
-vector<vector<T>> warshall(vector<vector<edge<T>>> &g){
+vector<vector<long long>> warshall(vector<vector<edge<T>>> &g){
+  const long long LINF = 1001002003004005006ll;
   int n = (int)g.size();
-  vector<vector<T>> d(n, vector<T> (n, numeric_limits<T>::max()));
+  vector<vector<long long>> d(n, vector<long long> (n, LINF));
   for(int i = 0; i < n; i++) d[i][i] = 0;
   for(int i = 0; i < n; i++) for(edge<T> e : g[i]) d[i][e.to] = e.cost;
   for(int k = 0; k < n; k++) for(int i = 0; i < n; i++) for(int j = 0; j < n; j++){
-    if(d[i][k] != numeric_limits<T>::max() && d[k][j] != numeric_limits<T>::max()){
+    if(d[i][k] != LINF && d[k][j] != LINF){
       d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
     }
   }
