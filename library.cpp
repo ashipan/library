7a71edad561d4b64217051900bb47ab32589e589
ashipan <r.hujiyama_0518@icloud.com>
2023-04-26 00:58:57 +0900

    fixed combination(fixed constructor, add perm function)

--- library.cpp
+++ library.cpp
@@ -479,7 +479,7 @@ mint mod_combination(int n, int r){
 struct modinv{
   int n;
   vector<mint> d;
-  modinv(): n(2), d({0, 1}) {};
+  modinv(): n(2), d({0, 1}) {}
   mint operator()(int i){ while(n <= i){ d.push_back(-d[mod%n]*(mod/n)); n++;} return d[i];}
   mint operator[](int i) const { return d[i];}
 }invs;
@@ -497,6 +497,7 @@ struct modfactinv{
   mint operator()(int i) { while(n <= i){ d.push_back(d.back()*invs(n)); n++;} return d[i];}
   mint operator[](int i) const { return d[i];}
 }ifacs;
+mint perm(int n, int k) { return facs(n)*ifacs(n-k); }
 mint comb(int a, int b){
   if(a < b || b < 0) return 0;
   return facs(a)*ifacs(b)*ifacs(a-b);
