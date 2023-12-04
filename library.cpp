c98398181a8276b1222b5aff75fb268cd536209f
ashipan <r.hujiyama_0518@icloud.com>
2023-12-04 17:01:46 +0900

    modified permutation_sum3

--- library.cpp
+++ library.cpp
@@ -2608,8 +2608,8 @@ ll permutation_sum(ll l, ll r, ll n){ return (l+r)*n/2;}
 mint mod_permutation_sum(ll l, ll r, ll n){ return mint(l+r)*n/2;}
 ll permutation_sum2(ll a, ll d, ll n){ return (2*a + (n-1)*d)*n/2;}
 mint mod_permutation2(ll a, ll d, ll n){ return mint(2*a + (n-1)*d)*n/2;}
-ll permutation_sum3(ll l, ll r, ll d=1){ return (l+r)*(r-l+1)/2;}
-mint mod_permutation_sum3(ll l, ll r, ll d=1){ return mint(l+r)*(r-l+1)/2;}
+ll permutation_sum3(ll l, ll r, ll d=1){ return (l+r)*((r-l)/d+1)/2;}
+mint mod_permutation_sum3(ll l, ll r, ll d=1){ return mint(l+r)*((r-l)d+1)/2;}
 
 
 //POWER_MODver. N^k % MOD
